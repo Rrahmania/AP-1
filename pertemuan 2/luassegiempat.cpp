37ff9040b897d19335d6031d2aebb45010848009
Rrahmania <rahmaniautami34@gmail.com>
2025-03-10 09:19:20 +0700

    Add files via upload

---
+++ pertemuan 2/luassegiempat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    float luas, p, l;
+
+    cout<<"Masukan Panjanng : ";
+    cin>>p;
+
+    cout<<"Masukan Lebar : ";
+    cin>>l;
+
+    luas = p*l;
+    cout <<"luas = "<<luas<<endl;
+    
+}
