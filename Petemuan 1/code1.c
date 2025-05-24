811546eff49760d4b608fbebc92732b01f5820bc
Rrahmania <rahmaniautami34@gmail.com>
2025-05-24 20:51:22 +0700

    code1.c

--- Petemuan 1/code1.c
+++ Petemuan 1/code1.c
@@ -1,4 +1,4 @@
-#include <stdio.h> //header untuk c
+#include <stdio.h> //header untuk c, digunakan untuk operasi input dan output standar, seperti printf(), scanf(), putchar(), dan getchar().
 #include <conio.h>
 
 int main(){
@@ -7,13 +7,13 @@ int main(){
     char kom;
     float ip;
 
-    printf("Hello World \n"); //keluaran pada c
+    printf("Hello World \n"); //keluaran pada c, untuk menampilkan kelayar
 
     printf("Masukan nama: ");
-    gets(nama); //get string
+    gets(nama); //get string, untuk membaca string dalam spasi 
 
     printf("Masukan nim :");
-    scanf("%d", &nim); //fungsi masukan pada c
+    scanf("%d", &nim); //fungsi masukan pada c, untuk mengambil data inputan
 
     printf("Masukan kom : ");
     gets(kom);
@@ -23,7 +23,7 @@ int main(){
 
     /*Untuk Output*/
     printf("Nama : ");
-    puts(nama); //put string digunakan dalam bentuk aray char
+    puts(nama); //put string digunakan dalam bentuk aray char, untuk menampilkan string dalam layar
 
     printf("Nim : %d\n ", nim);
 
