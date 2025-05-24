2900ae188f03832366ec52b1422a04ebb377e509
Rrahmania <rahmaniautami34@gmail.com>
2025-05-24 21:37:56 +0700

    operasikondisi.cpp

--- pertemuan 3/operasikondisi.cpp
+++ pertemuan 3/operasikondisi.cpp
@@ -9,19 +9,19 @@ int main() {
     cout<<"Masukan Nilai : ";
     cin>>nilai;
 
-    //if statment
+    //if statment, yaitu operasi kondisi ketika kondisi bernilai benar maka perintah akan di kerjakan jika tidak maka diabaikan.  
    /* if( nilai <= 65) {
         cout<<"Anda Tidak Lulus"<<endl;
     }*/
 
-    //if else statment
+    //if else statment, yaitu operasi kondisi jika kondisi bernilai benar maka perintah-1 akan dikerjakan dan tidak memenuhi syarat maka akan mengejerkan perintah-2.
    /* if (nilai <= 65) {
         cout <<"Anda Tidak Lulus" <<edl;
     } else {
         cout<<"Anda Lulus" <<endl;
     }*/
 
-    //if else if statment
+    //if else if statment, operasi kondisi jika kondisi bernilai benar maka peintah akan dijalankan jika tidak program akan mengecek kondisi lain dalam else if jika tidak sesuai kode else akan dijalankan.
    /* if (nilai == 100) {
         cout<<"Anda Kece"<<endl;
     } else (nilai <= 65) {
@@ -30,7 +30,7 @@ int main() {
         cout<<"anda lulus"endl;
     }*/
 
-    //nested if
+    //nested if, merupakn pernyataan if yang berada dalam pernyataan if yang lain.
    /* if (nilai <= 65) {
         cout<<"Anda Tidak Lulus" <<endl;
     } else {
@@ -41,7 +41,7 @@ int main() {
         }
     }*/
 
-    //switch case
+    //switch case, merupakan pernyataan yang dirancang khusus untuk pengambilan keputusan yang melibatkan sejumlah atau sebanyak alternatif.(hanya untuk data bertipe integer atau charakter).
     // switch (nilai)
     // {
     // case 1:
@@ -65,12 +65,12 @@ int main() {
     // case 7:
     //     cout<<"Minggu"<<endl;
     //     break;
-    // default: //apapun yang di luar dari case di beritahu
+    // default: //apapun yang di luar dari case di beritahu.
     //     cout <<"Inputan Tidak Valid"<<endl;
     //     break;
     // }
 
-    //switch range
+    //switch range, operasi kondisi dalam range tertentu.
     // switch(nilai){
     //     case 85 ... 100 : cout <<"A"<<endl; break;
     //     case 80 ... 84  : cout <<"B+"<<endl; break;
@@ -81,7 +81,7 @@ int main() {
     //     default : cout <<"E"<<endl; break;
     // }
 
-    //ternary Operator
+    //ternary Operator, yaitu operator yang bekerja dengan tiga operand: kondisi, nilai jika kondisi benar nilai jika kondisi salah.
     // if (nilai % 2 == 0) {
     //     cout<< "Genap nih" <<endl;
     // } else {
