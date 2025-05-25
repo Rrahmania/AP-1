1e5b385c9fc305826494cd07de1234f78b6abd3e
Rrahmania <rahmaniautami34@gmail.com>
2025-05-25 17:05:53 +0700

    struct.cpp

--- pertemuan 8/struct.cpp
+++ pertemuan 8/struct.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 
+// struct kumpulan variabel dengan tipe data yang berbeda dikelompokan jadi satu nama.
 struct Alamat{
     string jalan;
     string kota;
@@ -14,18 +15,16 @@ struct Mahasiswa
     string nama;
     int umur;
     float ipk;
-    Alamat alamat; //nested struct
+    Alamat alamat; //nested struct, yaitu struct dapat memiliki struct lain sebagai anggota.
 };
 
 int main()
 {
     system("CLS");
     Mahasiswa mhs1;
-
 //     mhs1.alamat.jalan = "Jalan Jamin Ginting";
 //     mhs1.alamat.kodePos = 21272;
 //     mhs1.alamat.kota = "medan";
-
 //     cout<<"Alamat : "<<mhs1.alamat.jalan<<" "<<mhs1.alamat.kota<<" "<<mhs1.alamat.kodePos<<endl;
 
 vector <Mahasiswa> mahasiswa;
@@ -46,7 +45,7 @@ for(int i = 0; i < n; i++){
     cout<<"Masukan ipk : ";
     cin>>mhs1.ipk;
 
-    mahasiswa.push_back(mhs1);
+    mahasiswa.push_back(mhs1); //untuk menambahkan elemen ke akhir
 }
 for (int i = 0; i < n; i++){
     cout << "Mahasiswa " << i + 1 <<endl;
@@ -54,5 +53,4 @@ for (int i = 0; i < n; i++){
     cout << "Umur : " << mahasiswa[i].umur<<endl;
     cout << "IPK : " << mahasiswa[i].ipk<<endl;
 }
-
 }
