23289c9baf94492275819802186e298946e1a30b
Rrahmania <rahmaniautami34@gmail.com>
2025-05-25 17:43:22 +0700

    Update latihan.cpp

---
+++ pertemuan 2/latihan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int r;
+    float volume, luas;
+    const float pi = 3.14;
+
+    cout << "Masukkan jari-jari bola: ";
+    cin >> r;
+
+    volume = (4.0 / 3.0) * pi * r * r * r;
+    luas = 4 * pi * r * r; 
+
+    cout << "Volume bola: " << volume << endl;
+    cout << "Luas permukaan bola: " << luas << endl;
+
+    return 0;
+}
