3e71733f6911c458bf5e92623ae3489a8c074252
Rrahmania <rahmaniautami34@gmail.com>
2025-05-25 16:31:41 +0700

    belajarfungsi.cpp

--- pertemuan 6/belajarfungsi.cpp
+++ pertemuan 6/belajarfungsi.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//parameter formal, para,eter yang menerima inputan pada saat fungsi dipanggil.
+//parameter formal, parameter yang menerima inputan pada saat fungsi dipanggil.
 void sapa(string nama) {
     cout << "Halo " << nama << "! Selamat belajar C++" << endl;
 }
