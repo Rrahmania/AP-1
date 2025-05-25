3cd7a920e938a8894b22117ffef51c0950f5bf8d
Rrahmania <rahmaniautami34@gmail.com>
2025-05-25 17:15:12 +0700

    Update perulangan.cpp

--- pertemuan 4/perulangan.cpp
+++ pertemuan 4/perulangan.cpp
@@ -58,7 +58,7 @@ int main(){
     //     cout<<endl;
     // }
 
-    //segitiga siku - siku 
+    //segitiga siku - siku, menggunakan nested loop.
     // for(int i = 1; i <=5; i++){
     //     for(int j = 1; j <= i; j++){
     //         cout<<" *";
